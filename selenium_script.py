96f4f378481c8ab60e7fe9c3613b781837f8189a
Ines Abdelaziz <66907261+Ines-Abdelaziz@users.noreply.github.com>
2025-07-08 16:49:49 +0200

    update script

--- selenium_script.py
+++ selenium_script.py
@@ -2,14 +2,14 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+
 def scrape_website(url, target_img_url):
     chrome_options = Options()
-    chrome_options.add_argument("--headless")  # run Chrome in headless mode (no GUI)
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
 
-    service = Service('/usr/local/bin/chromedriver')  # path where chromedriver is installed
-    driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver = webdriver.Chrome(options=chrome_options)
     try:
         driver.get(url)
         adurl = None
