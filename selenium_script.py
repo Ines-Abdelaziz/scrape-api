ef7701c8bb2ff32a555a76469cb25bd944717ecb
Ines Abdelaziz <66907261+Ines-Abdelaziz@users.noreply.github.com>
2025-07-08 16:02:02 +0200

    update chromedriver

--- selenium_script.py
+++ selenium_script.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 def scrape_website(url, target_img_url):
     chrome_options = Options()
     chrome_options.add_argument("--headless")  # run Chrome in headless mode (no GUI)
